a19ea825b2e1293cb8c9b4c2e20f8d7f12718fa6
Jan Halfar <jan@bestbytes.com>
2017-07-03 16:49:54 +0200

    yet another codegen reference point

--- demo/gotsrpc.go
+++ demo/gotsrpc.go
@@ -7,6 +7,72 @@ import (
 	time "time"
 )
 
+type FooGoTSRPCProxy struct {
+	EndPoint    string
+	allowOrigin []string
+	service     *Foo
+}
+
+func NewDefaultFooGoTSRPCProxy(service *Foo, allowOrigin []string) *FooGoTSRPCProxy {
+	return &FooGoTSRPCProxy{
+		EndPoint:    "/service/foo",
+		allowOrigin: allowOrigin,
+		service:     service,
+	}
+}
+
+func NewFooGoTSRPCProxy(service *Foo, endpoint string, allowOrigin []string) *FooGoTSRPCProxy {
+	return &FooGoTSRPCProxy{
+		EndPoint:    endpoint,
+		allowOrigin: allowOrigin,
+		service:     service,
+	}
+}
+
+// ServeHTTP exposes your service
+func (p *FooGoTSRPCProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	for _, origin := range p.allowOrigin {
+		// todo we have to compare this with the referer ... and only send one
+		w.Header().Add("Access-Control-Allow-Origin", origin)
+	}
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	if r.Method != http.MethodPost {
+		if r.Method == http.MethodOptions {
+			return
+		}
+		gotsrpc.ErrorMethodNotAllowed(w)
+		return
+	}
+
+	var args []interface{}
+	funcName := gotsrpc.GetCalledFunc(r, p.EndPoint)
+	callStats := gotsrpc.GetStatsForRequest(r)
+	if callStats != nil {
+		callStats.Func = funcName
+		callStats.Package = "github.com/foomo/gotsrpc/demo"
+		callStats.Service = "Foo"
+	}
+	switch funcName {
+	case "Hello":
+		args = []interface{}{int64(0)}
+		err := gotsrpc.LoadArgs(args, callStats, r)
+		if err != nil {
+			gotsrpc.ErrorCouldNotLoadArgs(w)
+			return
+		}
+		executionStart := time.Now()
+		helloRet := p.service.Hello(int64(args[0].(float64)))
+		if callStats != nil {
+			callStats.Execution = time.Now().Sub(executionStart)
+		}
+		gotsrpc.Reply([]interface{}{helloRet}, callStats, r, w)
+		return
+	default:
+		http.Error(w, "404 - not found "+r.URL.Path, http.StatusNotFound)
+	}
+}
+
 type DemoGoTSRPCProxy struct {
 	EndPoint    string
 	allowOrigin []string
@@ -113,72 +179,6 @@ func (p *DemoGoTSRPCProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			callStats.Execution = time.Now().Sub(executionStart)
 		}
 		gotsrpc.Reply([]interface{}{testScalarInPlaceRet}, callStats, r, w)
-		return
-	default:
-		http.Error(w, "404 - not found "+r.URL.Path, http.StatusNotFound)
-	}
-}
-
-type FooGoTSRPCProxy struct {
-	EndPoint    string
-	allowOrigin []string
-	service     *Foo
-}
-
-func NewDefaultFooGoTSRPCProxy(service *Foo, allowOrigin []string) *FooGoTSRPCProxy {
-	return &FooGoTSRPCProxy{
-		EndPoint:    "/service/foo",
-		allowOrigin: allowOrigin,
-		service:     service,
-	}
-}
-
-func NewFooGoTSRPCProxy(service *Foo, endpoint string, allowOrigin []string) *FooGoTSRPCProxy {
-	return &FooGoTSRPCProxy{
-		EndPoint:    endpoint,
-		allowOrigin: allowOrigin,
-		service:     service,
-	}
-}
-
-// ServeHTTP exposes your service
-func (p *FooGoTSRPCProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	for _, origin := range p.allowOrigin {
-		// todo we have to compare this with the referer ... and only send one
-		w.Header().Add("Access-Control-Allow-Origin", origin)
-	}
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	if r.Method != http.MethodPost {
-		if r.Method == http.MethodOptions {
-			return
-		}
-		gotsrpc.ErrorMethodNotAllowed(w)
-		return
-	}
-
-	var args []interface{}
-	funcName := gotsrpc.GetCalledFunc(r, p.EndPoint)
-	callStats := gotsrpc.GetStatsForRequest(r)
-	if callStats != nil {
-		callStats.Func = funcName
-		callStats.Package = "github.com/foomo/gotsrpc/demo"
-		callStats.Service = "Foo"
-	}
-	switch funcName {
-	case "Hello":
-		args = []interface{}{int64(0)}
-		err := gotsrpc.LoadArgs(args, callStats, r)
-		if err != nil {
-			gotsrpc.ErrorCouldNotLoadArgs(w)
-			return
-		}
-		executionStart := time.Now()
-		helloRet := p.service.Hello(int64(args[0].(float64)))
-		if callStats != nil {
-			callStats.Execution = time.Now().Sub(executionStart)
-		}
-		gotsrpc.Reply([]interface{}{helloRet}, callStats, r, w)
 		return
 	default:
 		http.Error(w, "404 - not found "+r.URL.Path, http.StatusNotFound)
