485320000fe6049b2d9e01c3a0699a993f2e5a12
Jan Halfar <jan@bestbytes.com>
2017-07-24 19:50:43 +0200

    preparing reader test

--- reader_test.go
+++ reader_test.go
@@ -1,1 +1,36 @@
 package gotsrpc
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFoo(t *testing.T) {
+	ReaderTrace = true
+	serviceMap := map[string]string{
+		"/demo": "Demo",
+	}
+
+	packageName := "github.com/foomo/gotsrpc/demo"
+
+	pkg, parseErr := parsePackage([]string{os.Getenv("GOPATH")}, packageName)
+	assert.NoError(t, parseErr)
+
+	services, err := readServicesInPackage(pkg, packageName, serviceMap)
+
+	t.Log(services, err)
+
+	missingTypes := map[string]bool{}
+	for _, s := range services {
+		for _, m := range s.Methods {
+			collectStructTypes(m.Return, missingTypes)
+			collectStructTypes(m.Args, missingTypes)
+			collectScalarTypes(m.Return, missingTypes)
+			collectScalarTypes(m.Args, missingTypes)
+		}
+	}
+	//spew.Dump(services)
+
+}
