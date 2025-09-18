cc9490482da1f7b74039754940f14f0e0bf1ee8e
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2025-09-18 23:21:52 +0200

    feat: update usage

--- cmd/gotsrpc/gotsrpc.go
+++ cmd/gotsrpc/gotsrpc.go
@@ -6,35 +6,64 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
+	"time"
 
 	"github.com/foomo/gotsrpc/v2"
 	"github.com/foomo/gotsrpc/v2/config"
 )
 
 var (
-	version string
+	version        = "dev"
+	commitHash     = "n/a"
+	buildTimestamp = "n/a"
 )
 
 func usage() {
-	fmt.Println("Usage")
-	fmt.Println(os.Args[0], " path/to/build-config.yml")
-	flag.PrintDefaults()
+	fmt.Print(`gotsrpc
+
+Usage:
+  gotsrpc [options] [command] <config-file>
+
+Available Commands
+  version    Display version information
+
+Options:
+  -debug     Print debug information
+
+Examples:
+  $ gotsrpc path/to/gotsrpc.yaml
+`)
 }
 
 func main() {
 	flagDebug := flag.Bool("debug", false, "debug")
 
+	flag.Usage = usage
 	flag.Parse()
+
 	args := flag.Args()
-	if len(args) != 1 {
+	switch {
+	case len(args) == 0:
 		usage()
 		os.Exit(1)
-	}
-	if flag.Arg(0) == "version" {
-		fmt.Println(version)
+	case args[0] == "version":
+		if *flagDebug {
+			buildTime := buildTimestamp
+			if value, err := strconv.ParseInt(buildTimestamp, 10, 64); err == nil {
+				buildTime = time.Unix(value, 0).String()
+			}
+			fmt.Printf("Version: %s\nCommit: %s\nBuildTime: %s\n", version, commitHash, buildTime)
+		} else {
+			fmt.Println(version)
+		}
 		os.Exit(0)
+	case len(args) != 1:
+		usage()
+		os.Exit(1)
+	default:
+		gotsrpc.ReaderTrace = *flagDebug
 	}
-	gotsrpc.ReaderTrace = *flagDebug
 
 	var goRoot string
 	var goPath string
@@ -53,7 +82,7 @@ func main() {
 
 	conf, err := config.LoadConfigFile(args[0])
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "config load error, could not load config from", args[0], ":", err)
+		_, _ = fmt.Fprintln(os.Stderr, "config load error, could not load config from", args[0], ":", err)
 		os.Exit(2)
 	}
 
