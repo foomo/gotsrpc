41eba42fbab4ac16532c3c0d66e718509217e5c1
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-03-19 23:29:46 +0100

    fix: add deps

--- tests/package_test.go
+++ tests/package_test.go
@@ -8,6 +8,11 @@ import (
 )
 
 func TestTypecheck(t *testing.T) {
+	install := exec.CommandContext(t.Context(), "bun", "install")
+	install.Stdout = t.Output()
+	install.Stderr = t.Output()
+	require.NoError(t, install.Run())
+
 	cmd := exec.CommandContext(t.Context(), "bun", "run", "-b", "typecheck")
 	cmd.Stdout = t.Output()
 	cmd.Stderr = t.Output()
