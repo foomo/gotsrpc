478b47e2f257a87dcf9788386327a2840e4800e2
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-03-23 07:22:39 +0100

    fix: switch service list sorting

--- internal/model/servicelist.go
+++ internal/model/servicelist.go
@@ -1,9 +1,7 @@
 package model
 
-import "strings"
-
 type ServiceList []*Service
 
 func (sl ServiceList) Len() int           { return len(sl) }
 func (sl ServiceList) Swap(i, j int)      { sl[i], sl[j] = sl[j], sl[i] }
-func (sl ServiceList) Less(i, j int) bool { return strings.Compare(sl[i].Name, sl[j].Name) > 0 }
+func (sl ServiceList) Less(i, j int) bool { return sl[i].Name < sl[j].Name }
